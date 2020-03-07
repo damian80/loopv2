a494633727860e9f405466eca8b7329492dce0ac
Damian Ciasnocha <daneek80@gmail.com>
2020-03-07 06:00:26 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// pobierz button za pomaoca queryselector i przypisz wynik do zmiennej
+// stworz zmienna z numerem
+let number = 1;
+
+// pobierz button za pomoca queryselector i przypisz wynik do zmiennej
+const btn = document.querySelector("button");
+const list = document.querySelector("ul");
+// ustaw nasluchiwanie na click i stworz funkcje anonimowa
+// stworz obiekt li w funkcji jako zmienna
+btn.addEventListener("click", function() {
+  const li = document.createElement("li");
+  li.textContent = number;
+  //   dodajmy obiekt li
+
+  list.appendChild(li);
+  if (number % 3 == 0) {
+    li.classList.add("big");
+  }
+  number += 2;
+});
